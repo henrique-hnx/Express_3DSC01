302bea91e43be5c1820729aded09472e95e60567
henrikke <130366433+henrique-hnx@users.noreply.github.com>
2024-10-04 14:53:38 +0000

    style logo e linkar as pagina

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", function (req, res) {
 app.get("/sobre", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
 });
+
+// ROTA USANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
